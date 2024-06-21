00224dbaa0f028661923d47064d9bc4b9339d1f8
danisa juarez <danisajuarezbelen14@gmail.com>
2024-06-21 15:26:26 -0300

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="px-20 pt-14">
       <NavBar></NavBar>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,46 @@
+import Button from "./Button";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="flex justify-between font-bold">
+        <img src="/Vector.svg" alt="" />
+        <img className="w-[2px]" src="/Vector22.png" alt="" />
+        <div className="grid">
+          <a href="">ABOUT</a>
+          <a href="">SERVICES</a>
+          <a href="">TECHNOLOGIES</a>
+          <a href="">HOW TO</a>
+          <a href="">JOIN HYDRA</a>
+        </div>
+        <img className="w-[2px]" src="/Vector22.png" alt="" />
+        <div className="grid">
+          <a href="">F.A.Q</a>
+          <a href="">SITEMAP</a>
+          <a href="">CONDITIONS</a>
+          <a href="">LICENSES</a>
+        </div>
+        <img className="w-[2px]" src="/Vector22.png" alt="" />
+        <div className="">
+          <h1>SOCIALIZE WITH HYDRA</h1>
+          <div className="mt-9 flex space-x-6">
+            <img src="/facebook.svg" alt="" />
+            <img src="/twitter.svg" alt="" />
+            <img src="/linkedin.svg" alt="" />
+            <img src="/youtube.svg" alt="" />
+            <img src="/instagram.svg" alt="" />
+            <img src="/pinterest.svg" alt="" />
+          </div>
+          <Button variant="filled" className="mt-12">
+            BUILD YOUR WORD
+          </Button>
+        </div>
+      </div>
+      <h1 className="mt-28 flex items-center justify-center text-[14px] font-bold">
+        2023 © HYDRA LANDING PAGE - BY ZINE. E. FALOUTI - ALL RIGHTS RESERVED
+      </h1>
+    </>
+  );
+};
+
+export default Footer;
